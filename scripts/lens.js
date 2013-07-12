6388f1cb86a27584ebf52918253d652540928fa6
Austin Birch <mraustinbirch@gmail.com>
2013-07-12 13:27:05 +0100

    Basic version of plugin. Needs more testing

--- scripts/lens.js
+++ scripts/lens.js
@@ -2,10 +2,10 @@
   // default settings
   var defaults = {
     thumbnailOpacity: 0.3,
-    thumbnailName: function(path) {
+    thumbnailPath: function(path) {
       return path.replace('.', '-thumb.');
     },
-    retinaName: function(path) {
+    retinaPath: function(path) {
       return path.replace('.', '@2x.');
     }
   };
@@ -20,11 +20,107 @@
     this.init();
   };
 
-  // Initializes the plugin for this instance
-  Lens.prototype.init = function() {
-    console.log('lens init');
+  // Hides all of the gallery images
+  Lens.prototype._hideImages = function() {
+    this.$el.find('.lens-image-container').css('display', 'none');
   };
 
+  // Shows an image for the given index
+  Lens.prototype.showImage = function(index) {
+    var query = '[data-lens-index="' + index + '"]';
+    this._hideImages();
+
+    // fade out all of the thumbnails
+    this.$el.find('.lens-thumbnail').fadeTo('fast',
+        this.options.thumbnailOpacity);
+
+    this.$el.find('.lens-image-container' + query).fadeIn();
+
+    // fade in the correct thumbnail
+    this.$el.find('.lens-thumbnail' + query).fadeTo('fast', 1);
+  };
+
+  // Funnily named method for inserting the retina image divs into the
+  // gallery. They will only request an image if we are on a retina device.
+  Lens.prototype._insertRetinas = function() {
+    var self = this;
+    $.each(this.images, function(index, image) {
+      var query = '.lens-image-container[data-lens-index="' + index + '"]';
+      var template = '"<div class="lens-image retina"></div>';
+
+      // hacky, I know
+      var template = [
+        '<div class="lens-retina-wrapper">',
+        '<div class="lens-image retina"></div>',
+        '</div>'
+      ];
+      var template = template.join('');
+
+      var $imageWrapper = $('<div></div>').html(template).contents();
+      var $img = $imageWrapper.children('.lens-image');
+      var url = self.options.retinaPath(image.src);
+
+      // find the standard version
+      var $standard = self.$el.find(query).find('.lens-image').first();
+
+      // wrap the standard version in a wrapper so we can hide it
+      $standard.wrap('<div class="lens-standard-wrapper" />');
+
+      var $standardWrapper = self.$el.find(query).find('.lens-standard-wrapper').first();
+
+      // set the retina background
+      $img.css('background-image', 'url(' + url + ')');
+      $img.css('background-size', 'contain');
+
+      // insert our image
+      $imageWrapper.insertBefore($standardWrapper);
+    });
+  };
+
+  Lens.prototype._createThumbnails = function() {
+    var self = this;
+    var $container = this.$el.children('.lens-thumbnail-container');
+    if ($container.length < 1) {
+      this.$el.prepend('<div class="lens-thumbnail-container"></div>');
+      $container = this.$el.children('.lens-thumbnail-container');
+    }
+
+    $.each(this.images, function(index, image) {
+      var template = '<img class="lens-thumbnail">';
+      var $thumb = $('<div></div>').html(template).contents();
+      var src = self.options.thumbnailPath(image.src);
+      $thumb.attr('src', src);
+      $thumb.attr('data-lens-index', index);
+      $container.append($thumb);
+
+      $thumb.click(function() {
+        self.showImage.apply(self, [$(this).attr('data-lens-index')]);
+      });
+    });
+  };
+
+  // Initializes the plugin for this instance
+  Lens.prototype.init = function() {
+    this.images = {};
+
+    // create the image models and indexes (our data backed plugin)
+    var self = this;
+    this.$el.find('.lens-image-container').each(function(index, container) {
+      var image = {};
+      image.src = $(container).find('.lens-image').first().attr('data-image');
+      self.images[index] = image;
+      $(container).attr('data-lens-index', index);
+      self._hideImages();
+    });
+
+    // create our thumbnails
+    this._createThumbnails();
+
+    // insert our retina images
+    this._insertRetinas();
+
+    this.showImage(0);
+  };
 
   // Plugin wrapper for jQuery
   $.fn.lens = function(options) {
